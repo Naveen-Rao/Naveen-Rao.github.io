e8e5abdb9f282210d4e3148e8e91b78cc9eadfd2
Naveen-Rao <raonaveen3130@gmail.com>
2017-04-30 15:45:39 +0530

    changes

---
+++ script.js
@@ -0,0 +1,6 @@
+function display()
+{
+    alert_message = '';
+    alert_message = alert_message + document.getElementById('name').value;
+    alert("Thanks for contacting me  "+alert_message+"  i'll get back to you soon");
+}
